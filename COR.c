c6dba929b9b9f4a3e12125a2ff74eb455bdf4c95
Your Name <you@example.com>
2024-03-02 02:29:41 +0000

    matei o sopas xd

--- COR.c
+++ COR.c
@@ -105,6 +105,5 @@ int main(int argc, char *argv[]) // Recebe os argumentos do terminal, argc - con
         else
             join(ring, id, ip, tcp);
     }
-    printf("adoro o sopas");
     return 0;
 }
