0f7d2106d6e0f762d5facc6e780f4a8378c5c830
Your Name <you@example.com>
2024-03-17 03:47:27 +0000

    sopas late night adventures

--- COR.c
+++ COR.c
@@ -1,6 +1,8 @@
 #include "COR.h"
 
-int fd, errcode, udp_fd;
+int fd, errcode, udp_fd, newfd, counter;
+int succ_fd = -1, pred_fd = -1, my_fd = -1;
+fd_set rfds;
 ssize_t n;
 socklen_t addrlen;
 struct addrinfo hints, *res;
@@ -8,61 +10,6 @@ struct sockaddr_in addr;
 char buffer[1028];
 char udp_buffer[1028];
 char nodes[10];
-
-char *TCP_Client(char *server_ip, char *server_port, char *msg)
-{
-    struct addrinfo hints, *res;
-    int fd, n;
-    ssize_t nbytes, nleft, nwritten, nread;
-    char *ptr, buffer[128 + 1];
-
-    fd = socket(AF_INET, SOCK_STREAM, 0); // TCP socket
-    if (fd == -1)
-        exit(1); // error
-
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_INET;       // IPv4
-    hints.ai_socktype = SOCK_STREAM; // TCP socket
-
-    n = getaddrinfo("127.0.0.1", "58001", &hints, &res); // IP e TCP do successor
-    if (n != 0)                                          /*error*/
-        exit(1);
-
-    n = connect(fd, res->ai_addr, res->ai_addrlen);
-    // perror("");
-    if (n == -1) /*error*/
-        exit(1);
-
-    ptr = strcpy(buffer, msg);
-    nbytes = strlen(msg);
-    nleft = nbytes;
-    while (nleft > 0)
-    {
-        nwritten = write(fd, ptr, nleft);
-        if (nwritten <= 0) /*error*/
-            exit(1);
-        nleft -= nwritten;
-        ptr += nwritten;
-    }
-    nleft = nbytes;
-    ptr = buffer;
-    while (nleft > 0)
-    {
-        nread = read(fd, ptr, nleft);
-        if (nread == -1) /*error*/
-            exit(1);
-        else if (nread == 0)
-            break; // closed by peer
-        nleft -= nread;
-        ptr += nread;
-    }
-    nread = nbytes - nleft;
-
-    buffer[nread] = '\0';
-    printf("echo: %s\n", buffer);
-    close(fd);
-    exit(0);
-}
 
 void UDP_client(char *msg)
 {
@@ -93,11 +40,115 @@ void UDP_client(char *msg)
     if (n == -1)
         exit(1);
 
+    fprintf(stderr, "n: %ld\n", n);
+
     write(1, "echo: ", 6);
     write(1, buffer, n);
 
     freeaddrinfo(res);
     close(fd);
+
+    return;
+}
+
+void TCP_Client(char *ip, char *port, char *msg)
+{
+
+    fprintf(stderr, "ENTREI\n");
+
+    int fd, errcode;
+    ssize_t n;
+    socklen_t addrlen;
+    struct addrinfo hints, *res;
+    struct sockaddr_in addr;
+    char buffer[200];
+
+    memset(buffer, 0, 200);
+
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd == -1)
+        exit(1);
+
+    fprintf(stderr, "FD: %d\n", fd);
+
+    succ_fd = fd;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    errcode = getaddrinfo(ip, port, &hints, &res);
+
+    if (errcode != 0)
+        exit(1);
+
+    n = connect(fd, res->ai_addr, res->ai_addrlen);
+
+    if (n == -1)
+        exit(1);
+
+    n = write(fd, msg, strlen(msg));
+    if (n == -1)
+        exit(1);
+
+    // n=read(fd,buffer,200);
+    // if(n==-1) exit(1);
+
+    // write(1,"echo: ",6); write(1,buffer,n);
+
+    freeaddrinfo(res);
+    // close(fd);
+
+    return;
+}
+
+void TCP_Server(char *port)
+{
+    int fd, errcode, newfd;
+    ssize_t n;
+    socklen_t addrlen;
+    struct addrinfo hints, *res;
+    struct sockaddr_in addr;
+    char buffer[200];
+
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd == -1)
+        exit(1);
+
+    my_fd = fd;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    errcode = getaddrinfo(NULL, port, &hints, &res);
+    if (errcode != 0)
+        exit(1);
+
+    n = bind(fd, res->ai_addr, res->ai_addrlen);
+    if (n == -1)
+        exit(1);
+
+    if (listen(fd, 5) == -1)
+        exit(1);
+
+    /*while(1){
+        addrlen=sizeof(addr);
+        if((newfd=accept(fd,(struct sockaddr*)&addr,&addrlen))==-1) exit(1);
+
+        n=read(newfd,buffer,200);
+        if(n==-1) exit(1);
+        write(1,"received: ",10);write(1,buffer,n);
+
+        n=write(newfd,buffer,n);
+        if(n==-1) exit(1);
+
+        close(newfd);
+    }*/
+
+    freeaddrinfo(res);
+    // close(fd);
 
     return;
 }
@@ -109,14 +160,7 @@ int id_exists(char *node_list, char *id)
 
     while (token != NULL)
     {
-
         if (strcmp(token, id) == 0)
-        {
-            return 1; // ID exists
-        }
-
-        // If the lengths are different, the IDs are definitely different
-        if (strlen(token) == strlen(id) && strncmp(token, id, strlen(id)) == 0)
         {
             return 1; // ID exists
         }
@@ -124,6 +168,40 @@ int id_exists(char *node_list, char *id)
         token = strtok(NULL, " \n"); // Tokenize based on both space and newline
     }
     return 0; // ID doesn't exist
+}
+
+int __str_len(const char *str, int sep)
+{
+    int i;
+
+    i = 0;
+    while (str && str[i] && sep != str[i])
+        ++i;
+    return (i);
+}
+
+char **__ft_split(const char *str, int sep)
+{
+    char **strings;
+    static int size;
+    int i;
+    char *word;
+
+    i = 0;
+    word = NULL;
+    while (*str && sep == *str)
+        str++;
+    i = __str_len(str, sep);
+    if (i)
+        word = strndup(str, i);
+    if (word && ++size)
+        strings = __ft_split(str + i, sep);
+    else
+        strings = (char **)calloc((size + 1), sizeof(char *));
+    strings[size--] = word;
+    if (size < 0)
+        ++size;
+    return (strings);
 }
 
 int join(char *ring, char *id, char *ip, char *tcp, char *succID, char *succIP, char *succTCP)
@@ -134,70 +212,82 @@ int join(char *ring, char *id, char *ip, char *tcp, char *succID, char *succIP, 
 
     // Call UDP_client to send the message and receive node_list
     // char *node_list = UDP_client(nodes);
-    
+
     // Send the nodes message do the UDP Client
     UDP_client(nodes);
 
     char *node_list = buffer;
 
-    // fprintf(stderr, "LISTA: %s\n", node_list);
-
     // Process the node_list
-    //fprintf(stderr,"Received response:\n%s\n", node_list);
+    // fprintf(stderr, "Received response:\n%s\n", node_list);
 
     // Extract the succesor id, ip and tcp
-    if (node_list)
+
+    char **split = __ft_split(__ft_split(node_list, '\n')[1], ' ');
+
+    succID = split[0];
+    succIP = split[1];
+    succTCP = split[2];
+
+    if (succID == NULL)
     {
-    if (sscanf(node_list, "NODELIST %s %s %s %s", ring, succID, succIP, succTCP) == 1)
-    {
+        TCP_Server(tcp);
+
+        // Establish a TCP connection to the successor node
         char tcp_message[128];
-        snprintf(tcp_message, sizeof(tcp_message), "ENTRY %s %s %s", id, ip, tcp);
+        snprintf(tcp_message, sizeof(tcp_message), "REG %s %s %s", id, ip, tcp);
 
+        // If node is empty then the node is its own successor
         TCP_Client(id, tcp, tcp_message);
+    }
 
-        if (sscanf(node_list, "NODELIST %s %s %s %s", ring, succID, succIP, succTCP) == 4)
+    if (succID != NULL)
+    {
+        // Check if the joining ID already exists in the ring
+        if (id_exists(node_list, id))
         {
-            fprintf(stderr, "SUCCESSOR ID: %s\nSUCCESSOR IP: %s\nSUCCESSOR TCP PORT: %s\n", succID, succIP, succTCP);
+            fprintf(stderr, "ID %s already exists in the ring. Generating a new ID...\n", id);
 
-            // Check if the joining ID already exists in the ring
-            if (id_exists(node_list, id))
+            // Generate a new ID
+            srand(time(NULL));
+            int new_id = (rand() % 99) + 1; // Generate a random number between 1 and 99
+
+            snprintf(id, 3, "%02d", new_id); // Ensure id has enough space
+
+            fprintf(stderr, "GENERATED ID: %s\n", id);
+
+            // Now, we need to recheck if the new ID exists in the node list
+            // If it does, repeat the process until we find a unique ID
+            while (id_exists(node_list, id))
             {
-                fprintf(stderr, "ID %s already exists in the ring. Generating a new ID...\n", id);
+                fprintf(stderr, "New ID %s already exists in the ring. Generating another new ID...\n", id);
 
-                // Generate a new ID
-                srand(time(NULL));
-                int new_id = (rand() % 99) + 1; // Generate a random number between 1 and 99
-
-                snprintf(id, 3, "%02d", new_id); // Ensure id has enough space
-
-                fprintf(stderr, "GENERATED ID: %s\n", id);
-
-                // Now, we need to recheck if the new ID exists in the node list
-                // If it does, repeat the process until we find a unique ID
-                while (id_exists(node_list, id))
-                {
-                    fprintf(stderr, "New ID %s already exists in the ring. Generating another new ID...\n", id);
-
-                    new_id = (rand() % 99) + 1;
-                    snprintf(id, 3, "%02d", new_id);
-                }
-
-                fprintf(stderr, "Final ID: %s\n", id);
+                new_id = (rand() % 99) + 1;
+                snprintf(id, 3, "%02d", new_id);
             }
 
-            fprintf(stderr, "Your new ID is: %s\n", id);
-
-            // Establish a TCP connection to the successor node
-            char tcp_message[128];
-            snprintf(tcp_message, sizeof(tcp_message), "ENTRY %s %s %s", id, ip, tcp);
-            TCP_Client(succIP, succTCP, tcp_message);
+            fprintf(stderr, "Final ID: %s\n", id);
         }
 
-        else
-        {
-            fprintf(stderr, "Failed to extract ID, IP, and TCP from the node list.\n");
-        }
+        TCP_Server(tcp);
+
+        // Establish a TCP connection to the successor node
+        char tcp_message[128];
+        snprintf(tcp_message, sizeof(tcp_message), "REG %s %s %s %s", ring, id, ip, tcp);
+
+        fprintf(stderr, "LOCAL REG: %s\n", tcp_message);
+
+        TCP_Client(succIP, succTCP, tcp_message);
+
+        fprintf(stderr, "BAZEI DO TCP\n");
     }
+
+    char registo[128];
+    snprintf(registo, sizeof(registo), "REG %s %s %s %s", ring, id, ip, tcp);
+
+    fprintf(stderr, "LOCAL REG: %s\n", registo);
+
+    UDP_client(registo);
 
     return 0;
 }
@@ -228,10 +318,10 @@ int main(int argc, char *argv[]) // Recebe os argumentos do terminal, argc - con
     //     regUDP = argv[4];
 
     // Debug messages
-    printf("IP: %s\n", argv[1]);
-    printf("TCP: %s\n", argv[2]);
-    printf("regIP: %s\n", regIP);
-    printf("regUDP: %s\n", regUDP);
+    // printf("IP: %s\n", argv[1]);
+    // printf("TCP: %s\n", argv[2]);
+    // printf("regIP: %s\n", regIP);
+    // printf("regUDP: %s\n", regUDP);
 
     char msg[128], ring[4], id[3], ip[13], tcp[6], command[3], succID[3], succIP[13], succTCP[6];
 
@@ -255,11 +345,51 @@ int main(int argc, char *argv[]) // Recebe os argumentos do terminal, argc - con
         {
             // Hardcode do ip e tcp do no a registar
             char *ip = "127.0.0.1";
-            char *tcp = "61000";
+            char *tcp = "60001";
 
             join(ring, id, ip, tcp, succID, succIP, succTCP);
         }
     }
+
+    // while (1)
+    //{
+    //     fd = Biggest_fd();
+    //     printf("\n");
+    //     FD_ZERO(&rfds);         /* remover todos os descritores do conjunto */
+    //     FD_SET(0, &rfds);       /* adicionar o descritor 0 (stdin) ao conjunto */
+    //     FD_SET(my_fd, &rfds);   /* adicionar o descritor fd (socket TCP) ao conjunto */
+    //     FD_SET(pred_fd, &rfds); /* adicionar o descritor 0 (stdin) ao conjunto */
+    //     FD_SET(succ_fd, &rfds); /* adicionar o descritor fd (socket TCP) ao conjunto */
+    //
+    //    counter = select(fd + 1, &rfds, (fd_set *)NULL, (fd_set *)NULL, (struct timeval *)NULL);
+    //    if (counter == -1)
+    //        exit(1);
+    //
+    //    if (FD_ISSET(0, &rfds) != 0)
+    //    {
+    //        if (Read_input() != 0)
+    //        {
+    //            break;
+    //        }
+    //    }
+    //    if (FD_ISSET(my_fd, &rfds) != 0)
+    //    {
+    //        addrlen = sizeof(addr);
+    //        if ((newfd = accept(my_fd, (struct sockaddr *)&addr, &addrlen)) == -1)
+    //            exit(1);
+    //        Read_buffer_tcp(newfd);
+    //    }
+    //    if (FD_ISSET(pred_fd, &rfds) != 0)
+    //    {
+    //        Read_buffer_tcp(pred_fd);
+    //    }
+    //    if (FD_ISSET(succ_fd, &rfds) != 0)
+    //    {
+    //        Read_buffer_tcp(succ_fd);
+    //    }
+    //    // printf("new cycle");
+    //}
+
     free(regIP);
     free(regUDP);
 
--- COR.h
+++ COR.h
@@ -10,25 +10,33 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <string.h>
+#include <strings.h>
 #include <stdlib.h>
 #include <time.h>
+
+
 #define PORT "59000"
 #define MAX_NODES 10 // Maximum number of nodes in the list
 
-typedef struct Node
-{
-    int id;
-    int TCP;
-    int pred;
-    int succ;
-    int succsucc;
-}Node;
+// variáveis globais
+extern char my_id[3], my_ip[16], my_port[6], succ_id[3], succ_ip[16], succ_port[6], succsucc_id[3], succsucc_ip[16], succsucc_port[6], pred_id[3];
+extern int succ_fd, pred_fd, my_fd;
 
 
-typedef struct Ring
-{
-    Node *nodelist;
-}Ring;
+// funções de criação de client UDP
+void UDP_client(char *msg);
+
+// funções de criação de client/server TCP
+void TCP_Client(char *ip, char *port, char *msg);
+void TCP_Server(char *port);
+
+// funções de leitura de buffer
+void Read_buffer_space_udp(char *buffer, char *id, char *ip, char *port);
+void Read_buffer_LF(char *buffer, char *id, char *ip, char *port);
+int Read_buffer_Nodeslist(char *buffer);
+int Succ_from_Nodeslist(char *buffer);
+void Read_buffer_tcp(int fd);
+
 
 
 #endif
